51bace4af70c4b93dc20ca823080217f6332e260
Algys <al_buldak@mail.ru>
2018-05-05 11:35:36 +0900

    Create main class and layout

---
+++ app/src/main/java/com/example/algys/allevents/main_activity.java
@@ -0,0 +1,19 @@
+package com.example.algys.allevents;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by Algys on 04.05.2018.
+ */
+
+public class main_activity extends Activity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
